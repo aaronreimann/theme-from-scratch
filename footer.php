56e26cdad3a0887d0d5cd9ae838ddeaf41e8a341
Aaron Reimann <aaron.reimann@gmail.com>
2016-03-03 12:39:40 -0500

    Fixes footer and adds sidebar.php

--- footer.php
+++ footer.php
@@ -15,5 +15,6 @@
 </footer>
 
 <?php wp_footer(); ?>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 get_header();
 ?>
 <div class="row">
-           <div class="columns small-8">
+	<div class="columns small-8">
 <?php
 if ( have_posts() ) {
 	while ( have_posts() ) {
@@ -16,7 +16,12 @@ if ( have_posts() ) {
 	}
 }
 ?>
-	</div><!-- closes small-8 -->
+	</div><!-- closes loop / small-8 -->
+	<div class="columns small-4">
+
+		<?php get_sidebar(); ?>
+
+	</div>
 </div>
 
-get_footer();
+<?php get_footer(); ?>
